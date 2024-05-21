6febb1273940948010b60de16ff356dfdf60b4ec
ens20mlr <104353331+ens20mlr@users.noreply.github.com>
2024-05-21 11:24:18 +0200

    EndScreen
    borde funka

--- GameOverScene.js
+++ GameOverScene.js
@@ -2,6 +2,14 @@ export default class GameOverScene extends Phaser.Scene {
   constructor() {
     super('gameOverScene');
     this.score;
+    this.messages = [
+      'Nu blev din hjärna glad',
+      'Tjohoo du gjorde det!',
+      'Bra kämpat!',
+      'Starkt jobbat! Varje utmaning gör dig bättre',
+      'Fantastiskt! Din uthållighet lönar sig.',
+      'Härligt kämpat! Din insats är fantastisk'
+    ];
   }
 
   init(data) {
@@ -10,19 +18,48 @@ export default class GameOverScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('bakgrund', 'bakgrundspil.png');
+    this.load.image('bakgrundkub', 'bakgrundskub.png'); 
+    this.load.image('knapp', 'knapp.png'); 
+    this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
 
   }
 
   create() {
-    // Display the "Game Over!" text
-    this.add.text(400, 190, 'Game Over!', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
-    this.add.text(400, 220, 'Hej på dig :)', { fontSize: '28px', fill: '#fff' }).setOrigin(0.5);
+    this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'bakgrund');
+    this.background.setOrigin(0);
+    this.cameras.main.setBackgroundColor('#D7F8FF');
 
-    const startButton = this.add.text(400, 300, 'Start', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
-    startButton.setInteractive();
-    startButton.on('pointerdown', () => {
-        // Resume the main game scene when the start button is clicked
-        this.scene.start('startScene');
+    WebFont.load({
+        google: {
+            families: ['Jaldi']
+        },
+        active: () => {
+            const textStyle = {
+                fontFamily: 'Jaldi',
+                fontSize: '30px',
+                color: '#1E2665',
+                align: 'center',
+                padding: { x: 50, y: 10 },
+            };
+
+            
+            const mittBilden = this.add.image(this.scale.width / 2, this.scale.height / 2, 'bakgrundkub').setOrigin(0.5);
+
+            const startButton = this.add.image(this.scale.width / 2, this.scale.height / 2 + 247, 'knapp').setOrigin(0.5);
+            startButton.setInteractive();
+            startButton.on('pointerdown', () => {
+                this.scene.start('startScene');
+            });
+            const randomMessage = this.messages[Math.floor(Math.random() * this.messages.length)];
+            this.add.text(this.scale.width / 2, 210, randomMessage, textStyle).setOrigin(0.5);
+
+        }
     });
-  }
+}
+
+update(time, delta) {
+  this.background.tilePositionX += 0.5;
+}
+
 }
